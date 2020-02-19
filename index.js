b14164a0e26af505074b5d996a123f61bbde92d7
Jonathan <Hannerfeldt@hotmail.se>
2020-02-19 11:19:10 +0100

    dsaniodas

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const { app, port } = require('./src/server')
+const mongoose = require('mongoose')
+const config = require('./config/config')
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser:true
+}
 
+mongoose.connect(config.databaseURL, options)
 app.listen(port, ()=> console.log(`server live at ${port}`))
